8d05723869cb4ff9ea9239411f4e779d8f4bd67c
aidensV <nameblacllis@gmail.com>
2019-09-23 22:25:44 +0700

    f

--- database/connection.go
+++ database/connection.go
@@ -10,5 +10,5 @@ func ConnectToDB(dbUser string, dbPasssword string, dbName string) (*gorm.DB, er
 	// 	"%s:%s@/%s?charset=utf8mb4&parseTime=True&loc=Local",
 	// 	dbUser, dbPasssword, dbName,
 	// )
-	return gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=gorm_crud_example password=postgres")
+	return gorm.Open("postgres", "host=ec2-54-204-37-92.compute-1.amazonaws.com port=5432 user=wfsobbobgfkelc dbname=d6fohcvmgj79rh password=785e47377465d41e2b515cd71cdbaf7ba72107b175d0d51c90f72bd2d22f14be")
 }
